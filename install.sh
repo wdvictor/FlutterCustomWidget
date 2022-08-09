dab5e212ea7825e73cea1964ee146adae484789a
victor.hugo <victorhpantunes@gmail.com>
2022-08-09 18:51:13 -0300

    Editing install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
-sed -i "$ a export FLUTTERCW_DIR=$(pwd)" ~/.text
-sed -i "$ a alias fluttercw=$FLUTTERCW_DIR/fluttercw.sh" ~/.text
-exit 0
+sed -i "$ a export FLUTTERCW_DIR=$(pwd)" ~/.bashrc
+sed -i "$ a alias fluttercw=$FLUTTERCW_DIR/fluttercw.sh" ~/.bashrc
