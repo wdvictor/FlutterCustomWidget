f2b4972cc92cb7248adfad2805ebf13c374822af
victor.hugo <victorhpantunes@gmail.com>
2022-08-09 19:01:56 -0300

    Adding installation return in install.sh file

--- install.sh
+++ install.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
+
+chmod +x ./fluttercw.sh
 sed -i "$ a export FLUTTERCW_DIR=$(pwd)" ~/.bashrc
 sed -i "$ a alias fluttercw=$FLUTTERCW_DIR/fluttercw.sh" ~/.bashrc
+
+echo '''fluttercw was installed successfully.
+
+Run fluttercw -h to see how to use.'''
